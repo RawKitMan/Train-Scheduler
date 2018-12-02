6b2bd58dcc573d411ccaa67babb3e0b257151a80
RawKitMan <cjvaughn@rocketmail.com>
2018-12-02 08:52:15 -0700

    Got the main functions set up. Will be adding a delete button and hopefully get the page to auto refresh every minute.

--- assets/javascript/train-schedule.js
+++ assets/javascript/train-schedule.js
@@ -1,40 +1,31 @@
 $(document).ready(function () {
 
-    
-    /*var timeA = moment();
-    var timeB = moment().add(500, "minutes")
-    console.log(timeA);
-    console.log(timeB);
-    console.log(moment.duration(timeB.diff(timeA, "m"), "m")._milliseconds/60000);*/
-    
+    let firstT = moment();
+    let nextT = moment(firstT);
+
     $("#submit-button").on("click", function () {
 
         event.preventDefault();
         const firstTrain = $("#first-time").val().trim();
 
-        const firstHour = parseInt(firstTrain.substr(0,2));
-        const firstMinutes = parseInt(firstTrain.substr(3,2));
-        console.log(firstHour);
-        console.log(firstMinutes);
-        const firstT= moment();
+        const firstHour = parseInt(firstTrain.substr(0, 2));
+        const firstMinutes = parseInt(firstTrain.substr(3, 2));
         const frequency = $("#frequency").val().trim()
         firstT.hours(firstHour);
         firstT.minutes(firstMinutes);
 
-        let nextT = moment(firstT).add(frequency, "m");
-        
-        let mAway = minutesAway();
+        nextT = moment(firstT).add(frequency, "m");
 
-        let trainInfo = {
+        var trainInfo = {
             name: $("#train-name").val().trim(),
             destination: $("#destination").val().trim(),
             firstTrain: firstT.format("HH:mm"),
             frequency: frequency,
-            nextTrain: nextT.format("HH:mm"),
-            minutesAway: mAway
+            nextMoment: nextT,
+            nextTrain: nextT.format("HH:mm")
         };
 
-        function minutesAway(){
+        /*function minutesAway(){
             let currentTime = moment();
             let minutesAway = moment.duration(nextT.diff(currentTime, "m"), "m")._milliseconds/60000;
             console.log(minutesAway);
@@ -42,23 +33,22 @@ $(document).ready(function () {
                 nextT.add(frequency, "m");
                 minutesAway = moment.duration(nextT.diff(currentTime, "m"), "m")._milliseconds/60000;
             };
-            console.log(nextT);
-            //trainInfo.nextTrain = nextT.format("HH:mm");
             return minutesAway;
-        }        
+        }*/
 
         $("#train-name").val("");
         $("#destination").val("");
         $("#first-time").val("");
         $("#frequency").val("");
 
+        trainInfo.minutesAway = minutesAway(trainInfo);
+        console.log(trainInfo);
+
         trains.push(trainInfo);
-        trains.forEach(function(obj){
-            obj.minutesAway = minutesAway();
-        });
+
         createSchedule(trains);
         sessionStorage.setItem("trainSchedule", JSON.stringify(trains));
-        
+
     });
 
     function createSchedule(arr) {
@@ -66,6 +56,8 @@ $(document).ready(function () {
         $("tbody").empty();
 
         arr.forEach(function (obj) {
+            console.log(obj.minutesAway);
+            console.log(obj.nextMoment);
             let tRow = $("<tr>");
             let tName = $("<td>");
             tName.text(obj.name);
@@ -75,10 +67,17 @@ $(document).ready(function () {
             tFirst.text(obj.firstTrain);
             let tFreq = $("<td>");
             tFreq.text(obj.frequency);
+
+            let newMoment = moment(obj.nextMoment);
+            obj.nextMoment = newMoment;
+            obj.minutesAway = minutesAway(obj);
+
+            
             let tNext = $("<td>");
             tNext.text(obj.nextTrain);
             let tAway = $("<td>");
             tAway.text(obj.minutesAway);
+
 
 
             tRow.append(tName, tDest, tFirst, tFreq, tNext, tAway);
@@ -87,19 +86,42 @@ $(document).ready(function () {
 
     }
 
-    
+    function minutesAway(obj) {
+        let currentTime = moment();
+        let minutesAway = moment.duration(obj.nextMoment.diff(currentTime, "m"), "m")._milliseconds / 60000;
+
+        while (minutesAway <= 0) {
+            obj.nextMoment = obj.nextMoment.add(obj.frequency, "m");
+            console.log(obj.nextMoment);
+            minutesAway = moment.duration(obj.nextMoment.diff(currentTime, "m"), "m")._milliseconds / 60000;
+            console.log(minutesAway);
+            obj.nextTrain = obj.nextMoment.format("HH:mm");
+        };
+
+
+        return minutesAway;
+    }
 
     //An array of objects that wiore train information. We want to pull it from local storage and parse it so it becomes an array again.
     let trains = JSON.parse(sessionStorage.getItem("trainSchedule"));
+
 
     //If there's nothing in the local storage to begin with, we create an empty array
     if (!Array.isArray(trains)) {
         trains = [];
     }
 
-    
 
 
+    if (trains.length > 0) {
+
+        trains.forEach(function (obj) {//add function here to reassign the moment
+
+            let newMoment = moment(obj.nextMoment);
+            obj.nextMoment = newMoment;
+            console.log(newMoment);
+        });
+    }
     //We want the table to show everything with current or no storage.
     createSchedule(trains);
 });
